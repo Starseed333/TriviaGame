95144c781f284a822a52fa20b1c392ca884da88c
Sandra Marquez <sandra@Sandras-MacBook-Air.local>
2017-08-22 20:55:38 -0500

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,26 @@
 console.log("inside js");
 
-var userPick;
-var correctAnswer = 0;
-var incorrectAnswer = 0;
-var unAnswer = 0;
-var question = 0;
-var question = 0;
-var images;
-var count = 30;
 
-var autoQuestion = [{
+
+$.fn.autoGame = function() {
+
+var game = this;
+
+	game.userSelection = null;
+ 	game.answers = {
+        correct: 0,
+        incorrect: 0
+    };
+    
+    game.images;
+    game.count = 30;
+    game.current = 0;
+
+    game.autoQuestion = [{
 		question: "In what year was the Call of Duty Jeep Wrangler vehicle made ?",
 		choices: ["2013", "2016", "2012", "2016"],
 		images: ["../images/jeepca.jpg"],
-		validAnswer: 2
+		correct: 2
 
 	},
 
@@ -21,14 +28,14 @@ var autoQuestion = [{
 		question: "In what year was the 100th year Anniversary Harley Davidson made ?",
 		choices: ["2002", "2003", "2000", "2005"],
 		images: ["../images/harleydavidsonca.jpg"],
-		validAnswer: 1
+		correct: 1
 	},
 
 	{ 
 		question: "In what year was the Ford Mustang Fastback Pit Viper made ?",
 		choices: ["1965", "1967", "1969", "1966"],
 		images: ["../images/mustangca.jpg"],
-		validAnswer: 1
+		correct: 1
 	},
 
 
@@ -36,73 +43,131 @@ var autoQuestion = [{
 		question: "In what year was the first Hummer H3T truck made ?",
 		choices: ["2005", "2008", "2006", "2010"],
 		images: ["../images/hummerca.jpg"],
-		validAnswer: 1
+		correct: 1
 	},
 
 	{ 
 		question: "In what year was the Porsche 911 Limited Edition Turbo S only 500 units made ?",
 		choices: ["2017", "2016", "2015", "2018"],
 		images: ["../images/nineeleven.jpg"],
-		validAnswer: 0
+		correct: 0
 	}
 
 
 ];
 
-	$("start_button").click(function(){
-		console.log("button clicked");
-		$(this).hide();
-		counter = setInterval(timer, 1000);
-		displayTrivia();
-	});
+game.ask = function() {
+        if (game.autoQuestion[game.current]) {
+            $("#timer").html("Time remaining: " + "00:" + game.count + " seconds");
+            $("#question_div").html(game.autoQuestion[game.current].question);
+            var choicesArr = game.autoQuestion[game.current].choices;
+            var buttonsArr = [];
 
-	
+            for (var i = 0; i < choicesArr.length; i++) {
+                var button = $('<button>');
+                button.text(choicesArr[i]);
+                button.attr('data-id', i);
+                $('#choices_div').append(button);
+            }
+            window.gameCounter = setInterval(game.timer, 1000);
+        } else {
+            $('body').append($('<div />', {
+                text: 'Unanswered: ' + (
+                    game.autoQuestion.length - (game.answers.correct + game.answers.incorrect)),
+                class: 'result'
+            }));
+            $('#start_button').text('Restart').appendTo('body').show();
+        }
+    };
 
 
-	function timer(){
-		count--;
-		if (count <=0) {
-		clearInterval(counter);
-		return;
-		}
-		$("#timer").html("Time remaing: " + "00:" + count + " secs");
-	}
-
- 	
 
 
- 	function displayTrivia(){
- 		$("question_div").html(autoQuestion[0].question);
- 		question++;
- 
+    game.timer = function() {
+        game.count--;
+        if (game.count <= 0) {
+            setTimeout(function() {
+                game.nextQuestion();
+            });
 
-	var choicesArr = autoQuestion[0].choices;
-	var buttonsArr = [];
+        } else {
+            $("#timer").html("Time remaining: " + "00:" + game.count + " seconds");
+        }
+    };
 
-	  for (let i = 0; i < choicesArr.length; i++) {
-    		var button = $('<button>');
-    		button.text(choicesArr[i]);
-    		button.attr('data-id', i);
-    		$('#choices_div').append(button);
-   	}
 
-  } 
 
- 			$('#choices_div').on("click", "button", function(e){
- 			userPick = $(this).data("id");
- 			autoQuestion[0].validAnswer;
- 			if(userPick != autoQuestion[0].validAnswer) {
+    game.nextQuestion = function() {
+        game.current++;
+        clearInterval(window.gameCounter);
+        game.count = 30;
+        $('#timer').html("");
+        setTimeout(function() {
+            game.cleanUp();
+            game.ask();
+        }, 4000)
+    };
 
- 			$('#choices_div').text("Wrong Answer! The correct answer was " + correct);
- 			incorrectAnswer++;
 
-		} 	else if (userPick === autoQuestion[0].validAnswer) {
-			$('#choices_div').text("Correct!!! The correct answer is " + correct);
-			correctAnswer++;
 
-}
 
+    game.cleanUp = function() {
+        $('div[id]').each(function(item) {
+            $(this).html('');
+        });
+        $('.correct').html('Correct answers: ' + game.answers.correct);
+        $('.incorrect').html('Incorrect answers: ' + game.answers.incorrect);
+    };
+
+
+
+
+    game.answer = function(correct) {
+        var string = correct ? 'Correct' : 'Incorrect';
+        game.answers[string]++;
+        $('.' + string).html(string + ' answers: ' + game.answers[string]);
+    };
+
+    return game;
+};
+
+
+
+
+
+var triviaGame;
+
+$("#start_button").click(function() {
+    $(this).hide();
+    $('.result').remove();
+    $('div').html('');
+    triviaGame = new $(window).autoGame();
+    triviaGame.ask();
 });
+
+
+
+
+
+
+
+$('#choices_div').on('click', 'button', function(e) {
+    var userSelection = $(this).data("id"),
+        game = triviaGame || $(window).game(),
+        index = game.autoQuestion[game.current].correct,
+        correct = game.autoQuestion[game.current].choices[index];
+
+    if (userSelection !== index) {
+        $('#choices_div').text("Wrong Answer! The correct answer was: " + correct);
+        game.answer(false);
+    } else {
+        $('#choices_div').text("Correct!!! The correct answer was: " + correct);
+        game.answer(true);
+    }
+    game.nextQuestion();
+});
+
+
 
 
 
